7810da77af291988bc07f11fb7e5f089ddc5c48e
Noora <Noora@DESKTOP-5E8MOLV>
2021-07-02 12:00:40 +0300

    Add project files.

---
+++ RyhmäprojektiVk2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RyhmäprojektiVk2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
